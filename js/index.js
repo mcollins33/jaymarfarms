64517e0727df99634a226af8576f916577b9db82
mcollins33 <collins.miki@gmail.com>
2018-03-19 09:04:57 -0400

    add autoplay to carousel

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 $( document ).ready(function(){
 
 	$(".button-collapse").sideNav();
+	$('.carousel').carousel();
+	setInterval(function() {
+    $('.carousel').carousel('next');
+  }, 3000); 
 	
 })
